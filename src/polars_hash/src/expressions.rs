575c6e56841b40c6b7c9c6ef8334211aedcc5c84
ion-elgreco <ioncjk@gmail.com>
2023-10-26 18:23:14 +0200

    formatting

--- src/polars_hash/src/expressions.rs
+++ src/polars_hash/src/expressions.rs
@@ -1,9 +1,7 @@
 use polars::prelude::*;
-// use polars_plan::dsl::FieldsMapper;
 use pyo3_polars::derive::polars_expr;
-// use serde::Deserialize;
-use std::fmt::Write;
 use sha256::digest;
+use std::fmt::Write;
 
 fn sha256_hash(value: &str, output: &mut String) {
     let hash = digest(value);
@@ -13,9 +11,6 @@ fn sha256_hash(value: &str, output: &mut String) {
 #[polars_expr(output_type=Utf8)]
 fn sha256(inputs: &[Series]) -> PolarsResult<Series> {
     let ca = inputs[0].utf8()?;
-    let out: Utf8Chunked = ca.apply_to_buffer(|value, output| 
-        sha256_hash(value, output)
-    
-);
+    let out: Utf8Chunked = ca.apply_to_buffer(|value, output| sha256_hash(value, output));
     Ok(out.into_series())
 }
