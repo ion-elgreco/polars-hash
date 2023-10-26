e15ce87735bc168de59693bde54c0a5cc8784e86
ion-elgreco <ioncjk@gmail.com>
2023-10-26 18:08:11 +0200

    functionally working now

--- src/polars_hash/polars_hash/__init__.py
+++ src/polars_hash/polars_hash/__init__.py
@@ -1,4 +1,18 @@
 import polars as pl
 from polars.utils.udfs import _get_shared_lib_location
 
-from polars_hash.hashing import 
+lib = _get_shared_lib_location(__file__)
+
+__version__ = "0.1.0"
+
+@pl.api.register_expr_namespace("stbl_hash")
+class HashingAlgorithms:
+    def __init__(self, expr: pl.Expr):
+        self._expr = expr
+
+    def sha256(self) -> pl.Expr:  # type: ignore
+        return self._expr._register_plugin(
+            lib=lib,
+            symbol="sha256",
+            is_elementwise=True,
+        )
--- src/polars_hash/src/expressions.rs
+++ src/polars_hash/src/expressions.rs
@@ -5,14 +5,17 @@ use pyo3_polars::derive::polars_expr;
 use std::fmt::Write;
 use sha256::digest;
 
-fn sha256_hash(value: &str, output: &mut String) -> String {
+fn sha256_hash(value: &str, output: &mut String) {
     let hash = digest(value);
-    write!(ouptut, "{hash}").unwrap()
+    write!(output, "{hash}").unwrap()
 }
 
 #[polars_expr(output_type=Utf8)]
 fn sha256(inputs: &[Series]) -> PolarsResult<Series> {
     let ca = inputs[0].utf8()?;
-    let out: Utf8Chunked = ca.apply_to_buffer(|value, output| sha256_hash(value, output));
+    let out: Utf8Chunked = ca.apply_to_buffer(|value, output| 
+        sha256_hash(value, output)
+    
+);
     Ok(out.into_series())
 }
