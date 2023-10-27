fa7a74d73093b9387a755fbfcd5a3483a1d47ca6
ion-elgreco <ioncjk@gmail.com>
2023-10-27 11:25:32 +0200

    change namespace, use workaround inspired by polars-business for type hinting

--- src/polars_hash/polars_hash/__init__.py
+++ src/polars_hash/polars_hash/__init__.py
@@ -1,32 +1,83 @@
 import polars as pl
 from polars.utils.udfs import _get_shared_lib_location
+from typing import Protocol, Iterable, cast
+from polars.type_aliases import PolarsDataType
 
 lib = _get_shared_lib_location(__file__)
 
 __version__ = "0.1.0"
 
-@pl.api.register_expr_namespace("stbl_hash")
-class HashingAlgorithms:
+
+@pl.api.register_expr_namespace("chash")
+class CryptographicHashingNameSpace:
     def __init__(self, expr: pl.Expr):
         self._expr = expr
 
-    def sha256(self) -> pl.Expr:  # type: ignore
+    def sha256(self) -> pl.Expr:
+        """Takes Utf8 as input and returns utf8 hash with sha256."""
         return self._expr._register_plugin(
             lib=lib,
             symbol="sha256",
             is_elementwise=True,
         )
-    
-    def xx64(self) -> pl.Expr:  # type: ignore
+
+
+@pl.api.register_expr_namespace("nchash")
+class NonCryptographicHashingNameSpace:
+    def __init__(self, expr: pl.Expr):
+        self._expr = expr
+
+    def xx64(self) -> pl.Expr:
+        """Takes Utf8 as input and returns uint hash with xx64."""
         return self._expr._register_plugin(
             lib=lib,
             symbol="xx64",
             is_elementwise=True,
         )
-        
-    def wyhash(self) -> pl.Expr:  # type: ignore
+
+    def wyhash(self) -> pl.Expr:
+        """Takes Utf8 as input and returns uint hash with wyhash."""
         return self._expr._register_plugin(
             lib=lib,
             symbol="wyhash",
             is_elementwise=True,
         )
+
+
+
+class HExpr(pl.Expr):
+    @property
+    def chash(self) -> CryptographicHashingNameSpace:
+        return CryptographicHashingNameSpace(self)
+    
+    @property
+    def nchash(self) -> NonCryptographicHashingNameSpace:
+        return NonCryptographicHashingNameSpace(self)
+
+
+class HashColumn(Protocol):
+    def __call__(
+        self,
+        name: str | PolarsDataType | Iterable[str] | Iterable[PolarsDataType],
+        *more_names: str | PolarsDataType,
+    ) -> HExpr:
+        ...
+
+    def __getattr__(self, name: str) -> pl.Expr:
+        ...
+
+    @property
+    def chash(self) -> CryptographicHashingNameSpace:
+        ...
+    
+    @property
+    def nchash(self) -> NonCryptographicHashingNameSpace:
+        ...
+
+
+col = cast(HashColumn, pl.col)
+
+
+__all__ = [
+    "col",
+]
--- src/tests/test_hash.py
+++ src/tests/test_hash.py
@@ -1,39 +1,43 @@
-import pytest
 import polars as pl
-import polars_hash
+import polars_hash as plh
 
 from polars.testing import assert_frame_equal
 
 
 def test_sha256():
-    result = pl.select(pl.lit('hello_world').stbl_hash.sha256())
-    
+    result = pl.select(pl.lit("hello_world").chash.sha256()) 
+
     expected = pl.DataFrame(
-    [
-        pl.Series("literal", ['35072c1ae546350e0bfa7ab11d49dc6f129e72ccd57ec7eb671225bbd197c8f1'], dtype=pl.Utf8),
-    ]
-)
-    
+        [
+            pl.Series(
+                "literal",
+                ["35072c1ae546350e0bfa7ab11d49dc6f129e72ccd57ec7eb671225bbd197c8f1"],
+                dtype=pl.Utf8,
+            ),
+        ]
+    )
     assert_frame_equal(result, expected)
-    
+
 
 def test_xx64():
-    result = pl.select(pl.lit('hello_world').stbl_hash.xx64())
-    
+    result = pl.select(pl.lit("hello_world").nchash.xx64())  # type: ignore
+
     expected = pl.DataFrame(
-    [
-        pl.Series("literal", ['5654987600477331689'], dtype=pl.Utf8),
-    ]
-)
-    
+        [
+            pl.Series("literal", ["5654987600477331689"], dtype=pl.Utf8),
+        ]
+    )
+
     assert_frame_equal(result, expected)
+
+
 def test_wyhash():
-    result = pl.select(pl.lit('hello_world').stbl_hash.wyhash())
-    
+    result = pl.select(pl.lit("hello_world").nchash.wyhash())  # type: ignore
+
     expected = pl.DataFrame(
-    [
-        pl.Series("literal", ['16737367591072095403'], dtype=pl.Utf8),
-    ]
-)
-    
+        [
+            pl.Series("literal", ["16737367591072095403"], dtype=pl.Utf8),
+        ]
+    )
+
     assert_frame_equal(result, expected)
