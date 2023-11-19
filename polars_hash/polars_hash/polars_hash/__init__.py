6947009fce932e355e8a8618314dcadf155cceb5
ion-elgreco <15728914+ion-elgreco@users.noreply.github.com>
2023-11-19 18:42:18 +0100

    bump pyo3-polars

--- polars_hash/polars_hash/polars_hash/__init__.py
+++ polars_hash/polars_hash/polars_hash/__init__.py
@@ -5,7 +5,7 @@ from polars.type_aliases import PolarsDataType, IntoExpr
 
 lib = _get_shared_lib_location(__file__)
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 @pl.api.register_expr_namespace("chash")
--- polars_hash/tests/test_hash.py
+++ polars_hash/tests/test_hash.py
@@ -18,19 +18,6 @@ def test_sha256():
     )
     assert_frame_equal(result, expected)
 
-
-# def test_xx64():
-#     result = pl.select(pl.lit("hello_world").nchash.xx64())  # type: ignore
-
-#     expected = pl.DataFrame(
-#         [
-#             pl.Series("literal", ["5654987600477331689"], dtype=pl.Utf8),
-#         ]
-#     )
-
-#     assert_frame_equal(result, expected)
-
-
 def test_wyhash():
     result = pl.select(pl.lit("hello_world").nchash.wyhash())  # type: ignore
 
