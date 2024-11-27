921999acaa9077042512c9b7809bfa43e99b7553
Moritz Wilksch <moritzwilksch@gmail.com>
2024-11-27 21:31:48 +0000

    swap hashing crates

--- polars_hash/src/fasthash_hashers.rs
+++ polars_hash/src/fasthash_hashers.rs
@@ -1,21 +1,28 @@
-use fasthash::{murmur3, xx};
+use mur3::murmurhash3_x64_128;
+use mur3::murmurhash3_x86_32;
+use xxhash_rust::xxh32::xxh32;
+use xxhash_rust::xxh64::xxh64;
 
 pub fn murmurhash3_32(value: Option<&str>, seed: u32) -> Option<u32> {
-    value.map(|v| murmur3::hash32_with_seed(v.as_bytes(), seed))
+    value.map(|v| murmurhash3_x86_32(v.as_bytes(), seed))
 }
 
 pub fn murmurhash3_128(value: Option<&str>, seed: u32) -> Option<Vec<u8>> {
     value.map(|v| {
-        murmur3::hash128_with_seed(v.as_bytes(), seed)
-            .to_le_bytes()
-            .to_vec()
+        let mut result = Vec::new();
+        let hash = murmurhash3_x64_128(v.as_bytes(), seed);
+
+        result.extend_from_slice(hash.0.to_le_bytes().as_ref());
+        result.extend_from_slice(hash.1.to_le_bytes().as_ref());
+
+        result
     })
 }
 
 pub fn xxhash_32(value: Option<&str>, seed: u32) -> Option<u32> {
-    value.map(|v| xx::hash32_with_seed(v.as_bytes(), seed))
+    value.map(|v| xxh32(v.as_bytes(), seed))
 }
 
 pub fn xxhash_64(value: Option<&str>, seed: u64) -> Option<u64> {
-    value.map(|v| xx::hash64_with_seed(v.as_bytes(), seed))
+    value.map(|v| xxh64(v.as_bytes(), seed))
 }
